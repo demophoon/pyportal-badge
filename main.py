e7fa4167f3af9f9d0783686b2055de50d5ad720d
Britt Gresham <britt@brittg.com>
2019-05-03 08:28:14 -0700

    Add doc strings to methods

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ reset_active = False
 
 
 def shuffle(l):
+    """
+    Return a randomly shuffled list given an ordered list.
+    """
     final_list = []
     for _ in l:
         final_list.append(l.pop(random.randrange(0, len(l))))
@@ -20,6 +23,14 @@ def shuffle(l):
 
 
 def in_region(region, point):
+    """
+    Detect if a point exists in a region.
+
+    Region: (x, y, width, height)
+    Point: (x, y)
+
+    Returns True or False depending on if the point exists in the region.
+    """
     rx, ry, rw, rh = region
     x, y = point
 
@@ -29,18 +40,31 @@ def in_region(region, point):
     )
 
 def main_screen():
+    """
+    Reset PyPortal background to primary screen
+    """
     pyportal.set_background('badge.bmp')
 
 
 def show_twitter():
+    """
+    Show twitter contact information.
+    """
     pyportal.set_background('twitter.bmp')
 
 
 def show_contact():
+    """
+    Show contact information.
+    """
     pyportal.set_background('contact.bmp')
 
 
 def show_calypso():
+    """
+    Randomly pick a picture of your good good kitty and show it on the screen!
+    :3
+    """
     global calypso_imgs
     if len(calypso_imgs) == 0:
         calypso_imgs = shuffle(os.listdir('/calypso'))
